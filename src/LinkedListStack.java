c0b7609074f9d11bd8ceae959d120bb1cbe94057
PunyaSharma <46519268+PunyaSharma@users.noreply.github.com>
2019-03-13 15:12:28 +0530

    Initial commit

---
+++ src/LinkedListStack.java
@@ -0,0 +1,58 @@
+public class LinkedListStack {
+
+    private static class Node {
+
+        private int data;
+        private Node next;
+
+        public Node (int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+        public int getData(){
+            return data;
+        }
+        public void setData (int data){
+            this.data = data;
+        }
+        public Node getNext(){
+            return next;
+        }
+        public void setNext(Node next){
+            this.next = next;
+        }
+
+    }
+
+    private Node top = null;
+    private  int size = 0;
+
+    public void push (int data){
+
+        Node newNode = new Node(data, this.top);
+        this.top = newNode;
+        this.size++;
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        stack.push(12);
+        System.out.println(stack);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        Node temp = this.top;
+        while (temp != null){
+            result.append(temp.getData());
+            if(temp.getNext() != null){
+                result.append(" --> ");
+            }
+            temp = temp.getNext();
+        }
+        result.append("]");
+        return result.toString();
+    }
+}
